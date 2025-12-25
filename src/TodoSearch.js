f3bab52fe36707b3a75e978889d6c28cfe126131
jorge <chauxjorge@hotmail.com>
2025-12-25 16:12:08 -0500

    Interacción con React: [Add] Estados en TodoSearch.js

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,42 +1,26 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+
+    // const [state, setState] = React.useState();
+    const [searchValue, setsearchValue] = React.useState("");
+
     function handleChange(event) {
-        console.log("ESCRIBISTE EN EL CAMPO DE TEXTO")
-        console.log("====> evento ", event)
-        console.log("====> target ", event.target)
-        console.log("====> Texto escrito ", event.target.value)
+        setsearchValue(event.target.value);
     }
+
+    console.log("Los usuarios buscan 'todos' de ", searchValue)
 
     return (
         <input
             placeholder="Cortar cebolla"
             className="TodoSearch"
+            value={searchValue}
             onChange={handleChange}
+            
         />
     );
 }
 
 export { TodoSearch };
-
-/*
-== ESTILO ARROW FUNCTION ==
-
-function TodoSearch() {
-    return (
-        <input
-            placeholder="Cortar cebolla"
-            className="TodoSearch"
-            onChange={
-                (event) => {
-                    console.log("ESCRIBISTE EN EL CAMPO DE TEXTO", )
-                    console.log("====> evento ", event)
-                    console.log("====> target ", event.target)
-                }
-            }
-        />
-    );
-}
-
-
-*/
