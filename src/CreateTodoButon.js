ad110c15bb6786b0b371dc31a233ce32a9315413
jorge <chauxjorge@hotmail.com>
2025-12-25 14:26:32 -0500

    Interacción con React: [Add] Eventos onClick, onChange

--- src/CreateTodoButon.js
+++ src/CreateTodoButon.js
@@ -1,8 +1,39 @@
 import './CreateTodoButton.css'
+
 function CreateTodoButon() {
+    function handleClick(event) {
+        console.log("DISTE CLICK!")
+        console.log("====> evento ", event)
+        console.log("====> target ", event.target)
+    }
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button
+            className="CreateTodoButton"
+            onClick={handleClick}
+        >+</button>
     );
 }
 
 export { CreateTodoButon };
+
+/*
+
+== ESTILO ARROW FUNCTION ==
+
+function CreateTodoButon() {
+
+    return (
+        <button
+            className="CreateTodoButton"
+            onClick={
+                (event) => {
+                    console.log("DISTE CLICK!")
+                    console.log("====> evento ", event)
+                    console.log("====> target ", event.target)
+                }
+            }
+        >+</button>
+    );
+}
+*/
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,42 @@
 import './TodoSearch.css';
 
 function TodoSearch() {
+    function handleChange(event) {
+        console.log("ESCRIBISTE EN EL CAMPO DE TEXTO")
+        console.log("====> evento ", event)
+        console.log("====> target ", event.target)
+        console.log("====> Texto escrito ", event.target.value)
+    }
+
     return (
         <input
             placeholder="Cortar cebolla"
             className="TodoSearch"
+            onChange={handleChange}
         />
     );
 }
 
 export { TodoSearch };
+
+/*
+== ESTILO ARROW FUNCTION ==
+
+function TodoSearch() {
+    return (
+        <input
+            placeholder="Cortar cebolla"
+            className="TodoSearch"
+            onChange={
+                (event) => {
+                    console.log("ESCRIBISTE EN EL CAMPO DE TEXTO", )
+                    console.log("====> evento ", event)
+                    console.log("====> target ", event.target)
+                }
+            }
+        />
+    );
+}
+
+
+*/
