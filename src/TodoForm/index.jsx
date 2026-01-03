7c290f361599b48823c19eb5b9533a0e75a13e29
jorge <chauxjorge@hotmail.com>
2026-01-03 11:37:46 -0500

    Herramientas avanzadas: [Add] Estilos css al modal

---
+++ src/TodoForm/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./TodoForm.css"
+
+function TodoForm() {
+    return (
+        <form>
+            <label>Escribe tu nuevo ToDo</label>
+            <textarea
+                placeholder="Cortar cebolla para el almuerzo"
+            />
+            <div className="TodoForm-buttonContainer">
+                <button
+                    className="TodoForm-button
+                    TodoForm-button--cancel"
+                >Cancelar</button>
+                <button
+                    className="TodoForm-button
+                    TodoForm-button--add"
+                >Añadir</button>
+            </div>
+        </form>
+    );
+}
+
+export { TodoForm };
