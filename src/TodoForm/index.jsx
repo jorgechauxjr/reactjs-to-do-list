ba9a3b52ed54a2e659533beb4f6559ea8c2c4007
jorge <chauxjorge@hotmail.com>
2026-01-04 18:02:30 -0500

    Maquetando formularios con React: [feature] cerrar modal con boton cancelar

--- src/TodoForm/index.jsx
+++ src/TodoForm/index.jsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { TodoContext } from "../TodoContext"
 import "./TodoForm.css"
 
 function TodoForm() {
+    const {
+        setOpenModal,
+
+    } = React.useContext(TodoContext);
+    
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setOpenModal(false);
+    }
+
+    const onCancel = () => {
+        setOpenModal(false);
+    }
+
     return (
-        <form onSubmit={(event) => {
-            event.preventDefault();
-        }}>
+        <form onSubmit={onSubmit}>
             <label>Escribe tu nuevo ToDo</label>
             <textarea
                 placeholder="Cortar cebolla para el almuerzo"
             />
             <div className="TodoForm-buttonContainer">
                 <button
-                    type="submit"
+                    type="button"
                     className="TodoForm-button
                     TodoForm-button--cancel"
+                    onClick={onCancel}
                 >Cancelar</button>
                 <button
+                    type="submit"
                     className="TodoForm-button
                     TodoForm-button--add"
                 >Añadir</button>
